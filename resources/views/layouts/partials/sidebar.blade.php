12ed6c7efc1d75087f80704adecc81e7b32d24d5
Mohamed Salah <conectsei165@gmail.com>
2025-03-19 23:32:51 +0200

    Initial commit

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,74 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <div class="sidebar-brand">
+        <a href="" class="brand-link">
+            <img src="{{ asset('dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image opacity-75 shadow" />
+            <span class="brand-text fw-light">AdminLTE 4</span>
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+        <nav class="mt-2">
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon bi bi-speedometer"></i>
+                        <p>Dashboard <i class="nav-arrow bi bi-chevron-right"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="" class="nav-link active">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Dashboard v1</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./index2.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Dashboard v2</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./index3.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Dashboard v3</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- بقية القائمة -->
+                <li class="nav-item">
+                    <a href="./generate/theme.html" class="nav-link">
+                        <i class="nav-icon bi bi-palette"></i>
+                        <p>Theme Generate</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-box-seam-fill"></i>
+                        <p>Widgets <i class="nav-arrow bi bi-chevron-right"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./widgets/small-box.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Small Box</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/info-box.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>info Box</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/cards.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Cards</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- يمكنك إضافة بقية القائمة هنا -->
+            </ul>
+        </nav>
+    </div>
+</aside>
